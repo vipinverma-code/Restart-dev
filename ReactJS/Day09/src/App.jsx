c03c0337b871fbed50bc7b3b3d647947b13a7940
vipinverma <vipinverma4224@gmail.com>
2026-09-12 21:14:51 +0530

    chnages done

--- ReactJS/Day09/src/App.jsx
+++ ReactJS/Day09/src/App.jsx
@@ -5,24 +5,41 @@ function App(){
     const[pass,setPass]=useState('')
     function handleSubmit(e){
         e.preventDefault();
-        console.log(setName(e.target.value));
-        console.log(setAge(e.target.value));
-        console.log(setPass(e.target.value));
-        
+        console.log(name);
+        console.log(age);
+        console.log(pass);   
     }
-    return(
-        <>
+
+    console.log("render");
+    return (
+      <>
         <form onSubmit={handleSubmit}>
+          <div>
             <div>
-            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
-            <input type="number" value={age} onChange={(e)=>setAge(e.target.value)} />
-            <input type="password" value={pass} onChange={(e)=>setPass(e.target.value)} />
-            <button type="submit">Submit</button>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
-            
+            <div>
+              <input
+                type="number"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}/>
+            </div>
+            <div>
+              <input
+                type="password"
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+              /> </div>
+
+            <button type="submit">Submit</button>
+          </div>
         </form>
-        </>
-    )
+      </>
+    );
 
 }
 export default App;
