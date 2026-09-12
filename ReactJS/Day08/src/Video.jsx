3caca9addfc6d6aa6ec2c1c9994a5f9b56fd2033
vipinverma <vipinverma4224@gmail.com>
2026-09-12 12:19:18 +0530

    changes done

--- ReactJS/Day08/src/Video.jsx
+++ ReactJS/Day08/src/Video.jsx
@@ -11,13 +11,22 @@ function Video(){
     function handleRestart(){
         videoRef.current.currentTime=0;
     }
+
+    function forward10(){
+        videoRef.current.currentTime+=10;
+    }
+    function backward10(){
+        videoRef.current.currentTime-=10;
+    }
     return(
         <>
-        <video ref={videoRef} src="/public/zidd.mp4" width="600" height="400" ></video>
+        <video ref={videoRef} src="/zidd.mp4" width="600" height="400" ></video>
         <div>
             <button onClick={handleStart}>start</button>
             <button onClick={handleStop}>pause</button>
             <button onClick={handleRestart}>Restart</button>
+            <button onClick={forward10}>+10</button>
+            <button onClick={backward10}>-10</button>
         </div>
         </>
     )
